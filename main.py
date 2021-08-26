e9106ab2e69b378b882dfe73969dba56845d4ae5
Juli Kuczkowska <julitakuczkowska@gmail.com>
2021-08-26 13:05:56 +0200

    bmi finish

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # wzór na bmi BMI = masa / wzrost2
 
-waga = input("podaj wagę:")
-wzrost = input("podaj wzrost:")
+waga = float(input("podaj wagę (kg):"))
+wzrost_cm = float(input("podaj wzrost (cm):"))
+wzrost = wzrost_cm / 100.0
+bmi = round((waga / wzrost**2), 2)
+
+print(f"Twoje BMI przy wadze {waga} i wzroście {wzrost} wynosi {bmi}")
